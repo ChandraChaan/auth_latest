f425a84e590b92104b3c4dadb2cac264d39c93e1
Chandra <dchandrachaan@gmail.com>
2023-06-24 09:00:47 +0530

    login changes

---
+++ lib/utility/app_text_feild.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppInputTextFeild extends StatelessWidget {
+  const AppInputTextFeild({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField();
+  }
+}
